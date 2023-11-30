0c2aed1497023903e21d2102bbbea7a33a46672e
Harshita Batra <101546087+Harshita-Batra-1@users.noreply.github.com>
2023-11-30 20:28:51 +0530

    Add files via upload

---
+++ news_alan_ai/build/precache-manifest.6c2a7d8bbf2c3a96302f7fa1bd99d984.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a0f9e2b2c49f529db6cbf2776028b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73bb6c77f37dfe2a1863",
+    "url": "/static/css/main.156fc103.chunk.css"
+  },
+  {
+    "revision": "639dc2864da2bd19acd5",
+    "url": "/static/js/2.a72be7f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a72be7f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73bb6c77f37dfe2a1863",
+    "url": "/static/js/main.387620df.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "252d1db77fa8b0be507e454a8a071da0",
+    "url": "/static/media/logo.252d1db7.png"
+  },
+  {
+    "revision": "ac137a708faff93e2016619ef8234cd8",
+    "url": "/static/media/logo2.ac137a70.png"
+  }
+]);
